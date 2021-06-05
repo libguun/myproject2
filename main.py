7b09bac8a45108aea7fcc1c1e72a1b35114d2106
libguun <78361280+libguun@users.noreply.github.com>
2021-06-05 18:42:53 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import modul as md
-import time
-from tkinter.filedialog import askopenfilename
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
